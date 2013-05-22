23095613f50df452a62704d1c50fd4a5955c80cf
Guido Accardo <gaccardo@gmail.com>
2013-05-22 16:03:16 -0300

    Menu es ahora dinámico y el theme está terminado de adaptar

--- web2py/applications/VamosViernes/controllers/Rosario.py
+++ web2py/applications/VamosViernes/controllers/Rosario.py
@@ -1,12 +1,15 @@
 # coding: utf8
 
 def index(): 
+   session.file = "index"
    return dict()
 
 def bares():
+   session.file   = "bares"
    response.flash = "VamosViernes Rosario"
    result         = list()
    bares          = db(db.bar.id).select() 
+   
 
    for bar in bares:
       comments = db( (db.comments.bar==bar.id)&(db.comments.uuser==db.auth_user.id) ).select()
