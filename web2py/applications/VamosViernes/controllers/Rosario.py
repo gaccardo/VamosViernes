01493fa6ecead6d3555b807b8e8aad098f0abd31
Guido Accardo <gaccardo@gmail.com>
2013-05-23 18:38:53 -0300

    Pagina para bares individuales

--- web2py/applications/VamosViernes/controllers/Rosario.py
+++ web2py/applications/VamosViernes/controllers/Rosario.py
@@ -17,3 +17,14 @@ def bares():
       result.append( {'bar':bar, 'comments':comments} )
 
    return dict(bares=result)
+
+def barsingle():
+   args     = request.args[0]
+   bar      = db( db.bar.name==args ).select()
+   bar      = bar[0]
+   comments = db( ( db.bar.id         == db.comments.bar ) &
+                  ( db.comments.uuser == db.auth_user.id)
+                ).select()
+   count    = len( comments )
+
+   return dict(bar=bar, comments=comments, count=count)
